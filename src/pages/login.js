3964f16ba43f62146e2e2c6f14e5828cf1da4820
skimmerbee <75343197+skimmerbee@users.noreply.github.com>
2024-09-22 15:05:00 +0530

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,7 +17,7 @@ const Login = () => {
 
     try {
       // Send login request to the server
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://co-laber.vercel.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }), // Send email and password
